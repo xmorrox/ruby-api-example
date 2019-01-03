57a8f516e674d5b3e817ad10dc90327b96e9f339
Joko Moro <joko.moro@gdn-commerce.com>
2019-01-03 10:53:42 +0700

    Update Item controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require 'rspec/rails'
 # require database cleaner at the top level
 require 'database_cleaner'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -46,6 +48,8 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include RequestSpecHelper, type: :request
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
